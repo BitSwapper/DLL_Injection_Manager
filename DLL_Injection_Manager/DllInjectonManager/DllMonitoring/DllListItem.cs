b240fa66e58a15e4148a19f111343dff4115c170
BitSwapper <killthirster@gmail.com>
2025-05-23 09:18:09 -0400

    Removed old application host

--- DLL_Injection_Manager/DllInjectonManager/DllMonitoring/DllListItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DLL_Injection_Manager.DllInjectonManager.DllMonitoring;
-
-public class DllListItem
-{
-    public string FullPath { get; }
-    public string FileName => Path.GetFileName(FullPath);
-
-    public DllListItem(string fullPath) => FullPath = fullPath ?? throw new ArgumentNullException(nameof(fullPath));
-}
